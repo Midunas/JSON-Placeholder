c6451809cd839d91874a52a68c445d26037b9411
Midunas <104984594+Midunas@users.noreply.github.com>
2022-07-08 09:05:52 +0300

    Update albums.js

--- JavaScript/albums.js
+++ JavaScript/albums.js
@@ -7,38 +7,36 @@ fetch(`https://jsonplaceholder.typicode.com/albums?_limit=30`)
         albums.map(album => {
 
             let userId = album.userId;
-            // (album)
+
 
             fetch('https://jsonplaceholder.typicode.com/users/'+ userId)
                 .then(res => res.json())
                 .then(user => {
 
 
-                    fetch(`https://jsonplaceholder.typicode.com/albums/${userId}/photos?_limit=1`)
+                    fetch(`https://jsonplaceholder.typicode.com/albums/${userId}/photos?_limit=10`)
                         .then(res => res.json())
                         .then(photos => {
 
-                             photos.map(photo => {
+                            let randomIndex = Math.floor(Math.random() * photos.length);
 
                                  let albumItem = document.createElement('div');
                                  albumItem.classList.add('album-item')
 
                                  let photoImage = document.createElement('img');
-                                 photoImage.src = photo.thumbnailUrl;
+                                 photoImage.src = photos[randomIndex].thumbnailUrl;
 
                                  let photoCount = document.createElement('p');
                                  photoCount.innerHTML = `(${photos.length} Photos)`;
 
                                  let userTitle = document.createElement('h4')
-                                 userTitle.innerHTML = `By: <br><br> ${user.name}`;
+                                 userTitle.innerHTML = `By: <br><br> <a href="./user.html?user_id=${user.id}">${user.name}</a>`;
 
                                  let albumTitle = document.createElement('h4');
-                                 albumTitle.innerHTML = `${album.title}`;
+                                 albumTitle.innerHTML = `<a href="./album.html?album_id=${album.id}&album_title${album.title}&user_id${album.userId}&user_name=${user.name}">${album.title}</a>`;
 
-                                albumItem.append(photoImage,albumTitle,userTitle);
+                                albumItem.append(photoImage,albumTitle,userTitle,photoCount);
                                 albumWrapper.append(albumItem);
-
-                             })
                         })
 
 

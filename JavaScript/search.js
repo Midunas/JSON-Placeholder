cebcbbc8757b68ab5728196618ebd1853ed0aaef
Midunas <mykolas.vidunas@codeacademylt.onmicrosoft.com>
2022-07-12 13:51:12 +0300

    functions in search page

--- JavaScript/search.js
+++ JavaScript/search.js
@@ -1,6 +1,3 @@
-let queryParams = document.location.search;
-let urlParams = new URLSearchParams(queryParams);
-let searchPhrase = urlParams.get("search-input");
 
 let searchResults = document.querySelector("#search-results-form");
 let usersList = document.createElement("ul");
@@ -18,7 +15,53 @@ usersList.before(usersListTitle);
 postsList.before(postsListTitle);
 albumsList.before(albumsListTitle);
 
-fetch(`https://jsonplaceholder.typicode.com/users?username=${searchPhrase}`)
+function init () {
+  innerPageSearchForm ()
+  outsidePageSearchForm()
+}
+
+function outsidePageSearchForm() {
+  let queryParams = document.location.search;
+  let urlParams = new URLSearchParams(queryParams);
+  let searchPhrase = urlParams.get("search-input");
+  
+    let namesUrl =`title=${searchPhrase}`;
+    renderAllNames(namesUrl);
+
+    let postsUrl = `title=${searchPhrase}`;
+    renderAllPosts(postsUrl);
+  
+    let albumsUrl = `title=${searchPhrase}`;
+    renderAllAlbums(albumsUrl);
+}
+
+function innerPageSearchForm () {
+
+  let searchPageForm = document.querySelector("#search-page-form");
+
+searchPageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  usersList.innerHTML = "";
+  postsList.innerHTML = "";
+  albumsList.innerHTML = "";
+
+  let searchInput = event.target.elements["search-input"].value;
+
+    let namesUrl = `_like=${searchInput}`;
+    renderAllNames(namesUrl);
+
+    let postsUrl = `title_like=${searchInput}`;
+    renderAllPosts(postsUrl);
+
+    let albumsUrl = `title_like=${searchInput}`;
+    renderAllAlbums(albumsUrl);
+});
+
+}
+
+function renderAllNames(searchText) {
+  fetch(`https://jsonplaceholder.typicode.com/users?username${searchText}`)
   .then((res) => res.json())
   .then((users) => {
     if (users.length > 0) {
@@ -31,7 +74,7 @@ fetch(`https://jsonplaceholder.typicode.com/users?username=${searchPhrase}`)
         usersList.append(userItem);
       });
     } else {
-      fetch(`https://jsonplaceholder.typicode.com/users?name=${searchPhrase}`)
+      fetch(`https://jsonplaceholder.typicode.com/users?name${searchText}`)
         .then((res) => res.json())
         .then((usersByName) => {
           if (usersByName.length > 0) {
@@ -45,7 +88,7 @@ fetch(`https://jsonplaceholder.typicode.com/users?username=${searchPhrase}`)
             });
           } else {
             fetch(
-              `https://jsonplaceholder.typicode.com/users?email=${searchPhrase}`
+              `https://jsonplaceholder.typicode.com/users?email${searchText}`
             )
               .then((res) => res.json())
               .then((usersByEmail) => {
@@ -67,137 +110,52 @@ fetch(`https://jsonplaceholder.typicode.com/users?username=${searchPhrase}`)
     }
   });
 
-fetch(`https://jsonplaceholder.typicode.com/posts?title=${searchPhrase}`)
-  .then((res) => res.json())
-  .then((posts) => {
-    if (posts.length > 0) {
-      postsListTitle.textContent = "Posts:";
-      posts.map((post) => {
-        let postItem = document.createElement("li");
-        postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title}</a>`;
+}
 
-        postsList.append(postItem);
-      });
-    } else {
-      postsListTitle.textContent = "Posts not found.";
-    }
-  });
-
-fetch(`https://jsonplaceholder.typicode.com/albums?title=${searchPhrase}`)
-  .then((res) => res.json())
-  .then((albums) => {
-    if (albums.length > 0) {
-      albumsListTitle.textContent = "Albums:";
-
-      albums.map((album) => {
-        let albumItem = document.createElement("li");
-        albumItem.innerHTML = `<a href="./album.html?album_id=${album.id}">${album.title}</a>`;
-
-        albumsList.append(albumItem);
-      });
-    } else {
-      albumsListTitle.textContent = "Albums not found.";
-    }
-  });
-
-let searchPageForm = document.querySelector("#search-page-form");
-
-searchPageForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  usersList.innerHTML = "";
-  postsList.innerHTML = "";
-  albumsList.innerHTML = "";
-
-  let searchInput = event.target.elements["search-input"].value;
-
-  fetch(
-    `https://jsonplaceholder.typicode.com/users?username_like=${searchInput}`
-  )
-    .then((res) => res.json())
-    .then((users) => {
-      if (users.length > 0) {
-        usersListTitle.textContent = "Users:";
-
-        users.map((user) => {
-          let userItem = document.createElement("li");
-          userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-
-          usersList.append(userItem);
-        });
-      } else {
-        fetch(
-          `https://jsonplaceholder.typicode.com/users?name_like=${searchInput}`
-        )
-          .then((res) => res.json())
-          .then((usersByName) => {
-            if (usersByName.length > 0) {
-              usersListTitle.textContent = "Users:";
-
-              usersByName.map((user) => {
-                let userItem = document.createElement("li");
-                userItem.classList.add("result-link");
-                userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-
-                usersList.append(userItem);
-              });
-            } else {
-              fetch(
-                `https://jsonplaceholder.typicode.com/users?email_like=${searchInput}`
-              )
-                .then((res) => res.json())
-                .then((usersByEmail) => {
-                  if (usersByEmail.length > 0) {
-                    usersListTitle.textContent = "Users:";
-
-                    usersByEmail.map((user) => {
-                      let userItem = document.createElement("li");
-                      userItem.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name}</a>`;
-
-                      usersList.append(userItem);
-                    });
-                  } else {
-                    usersListTitle.textContent = "Users not found.";
-                  }
-                });
-            }
-          });
-      }
-    });
-
-  fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${searchInput}`)
+function renderAllPosts(searchText) {
+  fetch(`https://jsonplaceholder.typicode.com/posts?${searchText}`)
     .then((res) => res.json())
     .then((posts) => {
       if (posts.length > 0) {
+
         postsListTitle.textContent = "Posts:";
         posts.map((post) => {
           let postItem = document.createElement("li");
           postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title}</a>`;
-
+  
           postsList.append(postItem);
         });
       } else {
         postsListTitle.textContent = "Posts not found.";
       }
     });
+}
 
-  fetch(`https://jsonplaceholder.typicode.com/albums?title_like=${searchInput}`)
-    .then((res) => res.json())
-    .then((albums) => {
-      if (albums.length > 0) {
-        albumsListTitle.textContent = "Albums:";
+function renderAllAlbums(searchText) {
+  fetch(`https://jsonplaceholder.typicode.com/albums?${searchText}`)
+  .then((res) => res.json())
+  .then((albums) => {
+    if (albums.length > 0) {
 
-        albums.map((album) => {
-          let albumItem = document.createElement("li");
-          albumItem.innerHTML = `<a href="./album.html?album_id=${album.id}">${album.title}</a>`;
-
-          albumsList.append(albumItem);
-        });
-      } else {
-        albumsListTitle.textContent = "Albums not found.";
-      }
-    });
-});
+      renderListElement(albums);
+    } else {
+      albumsListTitle.textContent = "Albums not found.";
+    }
+  });
 
 
-// .search-input : focus-visible { outline : none ; }
+}
+
+function renderListElement(list) {
+
+  albumsListTitle.textContent = "Albums:";
+
+  list.map((album) => {
+    let albumItem = document.createElement("li");
+    albumItem.innerHTML = `<a href="./album.html?album_id=${album.id}">${album.title}</a>`;
+
+    albumsList.append(albumItem);
+  });
+
+}
+init();
